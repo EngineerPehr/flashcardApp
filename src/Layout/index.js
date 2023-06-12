17b10d13d3216f0647ccccb435fc3229dfb4d9b8
EngineerPehr <dragontoothforge@gmail.com>
2023-06-12 09:43:19 -0600

    Added Home module to /src/index.js

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,14 +1,22 @@
-import React from "react";
-import Header from "./Header";
-import NotFound from "./NotFound";
+import React from "react"
+import { Switch, Route, Link} from 'react-router-dom'
+import Header from "./Header"
+import NotFound from "./NotFound"
+import Home from './Home'
 
 function Layout() {
   return (
     <>
       <Header />
       <div className="container">
-        {/* TODO: Implement the screen starting here */}
-        <NotFound />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
       </div>
     </>
   );
