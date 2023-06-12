20edcd3687a12b8692b6426f0ee93d1f8f2dcd2f
EngineerPehr <dragontoothforge@gmail.com>
2023-06-12 15:49:02 -0600

    Started Study

--- src/Layout/Study.js
+++ src/Layout/Study.js
@@ -1,8 +1,32 @@
 import React, { useState, useEffect} from "react"
-import { readCard } from "../utils/api"
+import { useParams } from "react-router-dom"
+import { readDeck } from "../utils/api"
 
 function Study () {
-    return <p>Study here</p>
+    const { deckId } = useParams()
+    const [deck, setDeck] = useState({})
+
+    useEffect(() => {
+        const abortcontroller = new AbortController()
+        const loadDeck = async () => {
+            const data = await readDeck(deckId)
+            setDeck(data)
+        }
+        loadDeck()
+        return () => {
+            abortcontroller.abort()
+        }
+    }, [deck])
+
+    return (
+        <>
+        <nav></nav>
+        <h3>{deck.name}</h3>
+        <p>Need Cards</p>
+        </>
+    )
+
+
 }
 
 export default Study
